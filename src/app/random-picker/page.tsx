a4a5d221c988e2245189d40aa05bd124d772776d
blcklamb <whenucan35@gmail.com>
2024-12-05 17:42:00 +0900

    🚧 random picker page

---
+++ src/app/random-picker/page.tsx
@@ -0,0 +1,11 @@
+import { Hammer } from "lucide-react";
+
+export default async function RandomPickerPage() {
+  return (
+    <div className="w-full h-full flex items-center justify-center gap-4">
+      <Hammer />
+      공사 중이에요
+      <Hammer />
+    </div>
+  );
+}
